3069f84cbd0969aef105e817b2cd5f1a1f67e298
Jason <kangja1996@gmail.com>
2021-10-22 22:29:50 -0400

    progress

---
+++ LinkedList.js
@@ -0,0 +1,24 @@
+const LinkedListNode = require("./LinkedListNode");
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  insert(data) {
+    const node = new LinkedListNode(data);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+  }
+}
+
+module.exports = LinkedList;
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const LinkedListNode = require("./LinkedListNode");
+// const LinkedListNode = require("./LinkedListNode");
 
-const someNode = new LinkedListNode(42);
-const someOtherNode = new LinkedListNode(13);
+// const someNode = new LinkedListNode(42);
+// const someOtherNode = new LinkedListNode(13);
+// const anotherNode = new LinkedListNode(7);
 
-someNode.next = someOtherNode;
+// someNode.next = someOtherNode;
+// someOtherNode.next = anotherNode;
+// console.log(someNode);
 
-console.log(someNode);
+const LinkedList = require("./LinkedList");
+
+const myList = new LinkedList();
+myList.insert(42);
+myList.insert(13);
+myList.insert(7);
+
+console.log(myList);
